0b88d2ed49481c9bb168b95ace6cc0664514ca11
Liyas Thomas <liyascthomas@gmail.com>
2020-01-29 03:59:37 +0530

    :green_heart: Fixing CI Build

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -8,7 +8,7 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
-export default {
+module.exports = {
   staticFileGlobs: ["src/**/*", "manifest.json"],
   runtimeCaching: [
     {
